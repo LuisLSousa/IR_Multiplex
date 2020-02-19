645047aba6c7b4f88277efd53375dcf4596b9762
LuisLSousa <luis.srl.sousa@gmail.com>
2020-02-19 16:48:55 +0000

    initiate Nodes and graph topology: random, wattsStrogatz

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ from utils import *
 class IndirectReciprocityMultiplexNetworks:
 
 	nodes = []
-
-	def __init__(self, numNodes=100):
+	def __init__(self, numNodes=100, graphType=None, prob=0.5, avgDegree=2):
 		self.numNodes = numNodes
-
-
-
+		self.graphType = graphType # Graph topology
+		self.prob = prob # Rewire probability for Watts-Strogatz
+		self.avgDegree = avgDegree
+		self.layer1 = None
+		self.layer2 = None
 
 		self.idIterator = 0
-		#self.idToIndex = {}  # id:index
-
+		self.idToIndex = {}  # id:index
 		self.initiateNodes()
 
 	def initiateNodes(self):
 
 		initialReputations = self.calculateInitialReputations()
 		initialStrategies = self.calculateInitialStrategies()
-
+		'''
 		for i in range(self.numNodes):
 			self.nodes.append({
 				'id': self.idIterator,
@@ -30,11 +30,33 @@ class IndirectReciprocityMultiplexNetworks:
 				'reputation': initialReputations[i],
 				'strategy': initialStrategies[i]
 			})
-			#self.idToIndex[self.idIterator] = len(self.nodes) - 1
+			self.idToIndex[self.idIterator] = len(self.nodes) - 1
 			self.idIterator += 1
+		'''
 
-			# Check reputations and strategies
-			#print('Node:', i, '| Reputation:', self.nodes[i]['reputation'], '| Strategy:', self.nodes[i]['strategy'])
+		if self.graphType == 'Random':
+			self.layer1 = MultiplexNetwork(self.numNodes, self.avgDegree)
+
+		elif self.graphType == 'WattsStrogatz':
+			self.layer1 = wattsStrogatz(self.numNodes, self.avgDegree, self.prob)
+
+			self.nodePos = list(self.layer1.nodes())
+			for i in range(self.numNodes):
+				self.nodes.append({
+					'pos': self.nodePos[i], # Redundante?
+					'id': self.idIterator,
+					'payoff': 0,
+					'reputation': initialReputations[i], # Update this so each node has his opinion about all others
+					'strategy': initialStrategies[i]
+				})
+
+				# Check reputations and strategies
+				#print('Node:', self.nodes[i]['id'], 'Pos:', self.nodes[i]['pos'], '| Reputation:', self.nodes[i]['reputation'], '| Strategy:', self.nodes[i]['strategy'])
+
+				self.idToIndex[self.idIterator] = len(self.nodes) - 1
+				self.idIterator += 1
+
+		print(self.layer1.nodes())
 
 	def calculateInitialReputations(self):
 		initialReputations = ['Good' if random.choice([0,1]) == 0 else 'Bad' for _ in
@@ -56,7 +78,7 @@ class IndirectReciprocityMultiplexNetworks:
 
 		return initialStrategies
 	
-
+'''
 	def reproduce_Social(self):  # social learning where nodes copy another node's strategy with a given probability if that node's payoff is higher
 		beta = 10
 		for pair in interactionPairs:
@@ -64,13 +86,20 @@ class IndirectReciprocityMultiplexNetworks:
 			partner = pair[1]
 			if partner['payoff'] > mine['payoff']:
 				prob = 1 / (1 + math.exp(-beta * (partner['payoff'] - mine['payoff'])))
-
+				if probability(prob):
+                	mine['strategy'] = partner['strategy']
+'''
 
 if __name__ == "__main__":
 	# Variables used
 	initialValues = {
-		'numNodes' : 100,
+		'numNodes': 100, # Number of nodes
+		'graphType': 'WattsStrogatz', # Graph topology: 'WattsStrogatz', 'Random', ...
+		'prob': 1, # Probability of rewiring links (WattsStrogatz)
+		'avgDegree': 2,
 	}
-	
+	# Adicionar graphType1 e 2 para o layer 1 e 2 respectivamente
+
 	config = initialValues.copy()
 	sim = IndirectReciprocityMultiplexNetworks(**config)
+
--- utils.py
+++ utils.py
@@ -1,3 +1,24 @@
 import networkx as nx
 import math
 import random
+
+class MultiplexNetwork(nx.Graph):
+    # Generate a random graph with a given average degree and size.
+    def __init__(self, numNodes, avgDegree):
+        super().__init__()
+        self.numNodes = numNodes
+        self.avgDegree = avgDegree
+        self.add_nodes_from(range(self.numNodes))
+        while self.avg_deg() < self.avgDegree:
+            n1, n2 = random.sample(self.nodes(), 2)
+            self.add_edge(n1, n2, weight=1)
+
+    def avg_deg(self):
+        return self.number_of_edges() * 2 / self.numNodes
+
+def wattsStrogatz(numNodes, avgDegree, prob, rndSeed=None):
+    G = nx.watts_strogatz_graph(numNodes,avgDegree, prob, seed=rndSeed)
+    return G
+
+def probability(percentage):
+    return (random.random() < percentage);
