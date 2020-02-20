bd9a968a61a6443ad0491de95535f8b732df8385
LuisLSousa <luis.srl.sousa@gmail.com>
2020-02-20 01:02:06 +0000

    Working: Layer1: Watts Strogatz, interactions, payoff update, reproduceSocial, drawGraph          Layer2: Not implemented Missing: Layer1: Neighbor updating his perception of the donor          Layer2: Gossip that information

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import numpy as np
 import random
+import os
 from os import mkdir
+from os.path import join
 from utils import *
+import json
+
 
 class IndirectReciprocityMultiplexNetworks:
 
 	nodes = []
-	def __init__(self, numNodes=100, graphType=None, prob=0.5, avgDegree=2):
+	def __init__(self, numNodes=100, prob=0.5, avgDegree=2, numGenerations=100, logFreq=1, cost=0.1, benefit=1, layer1=None, layer2=None):
 		self.numNodes = numNodes
-		self.graphType = graphType # Graph topology
 		self.prob = prob # Rewire probability for Watts-Strogatz
 		self.avgDegree = avgDegree
-		self.layer1 = None
-		self.layer2 = None
+		self.numGenerations=numGenerations
+		self.logFreq = logFreq
+		self.cost = cost
+		self.benefit = benefit
+
+		self.layer1 = layer1 # Layer1 topology
+		self.layer2 = layer2 # Layer2 topology
 
 		self.idIterator = 0
 		self.idToIndex = {}  # id:index
@@ -20,86 +28,170 @@ class IndirectReciprocityMultiplexNetworks:
 
 	def initiateNodes(self):
 
-		initialReputations = self.calculateInitialReputations()
 		initialStrategies = self.calculateInitialStrategies()
-		'''
-		for i in range(self.numNodes):
-			self.nodes.append({
-				'id': self.idIterator,
-				'payoff': 0,
-				'reputation': initialReputations[i],
-				'strategy': initialStrategies[i]
-			})
-			self.idToIndex[self.idIterator] = len(self.nodes) - 1
-			self.idIterator += 1
-		'''
 
-		if self.graphType == 'Random':
+		if self.layer1 == 'Random':
 			self.layer1 = MultiplexNetwork(self.numNodes, self.avgDegree)
 
-		elif self.graphType == 'WattsStrogatz':
+		elif self.layer1 == 'WattsStrogatz':
 			self.layer1 = wattsStrogatz(self.numNodes, self.avgDegree, self.prob)
 
-			self.nodePos = list(self.layer1.nodes())
-			for i in range(self.numNodes):
-				self.nodes.append({
-					'pos': self.nodePos[i], # Redundante?
-					'id': self.idIterator,
-					'payoff': 0,
-					'reputation': initialReputations[i], # Update this so each node has his opinion about all others
-					'strategy': initialStrategies[i]
-				})
+		else:
+			print('Wrong layer1 parameter!')
+			exit()
 
-				# Check reputations and strategies
-				#print('Node:', self.nodes[i]['id'], 'Pos:', self.nodes[i]['pos'], '| Reputation:', self.nodes[i]['reputation'], '| Strategy:', self.nodes[i]['strategy'])
+		if self.layer2 == 'Random':
+			self.layer2 = MultiplexNetwork(self.numNodes, self.avgDegree)
 
-				self.idToIndex[self.idIterator] = len(self.nodes) - 1
-				self.idIterator += 1
+		elif self.layer2 == 'WattsStrogatz':
+			self.layer2 = wattsStrogatz(self.numNodes, self.avgDegree, self.prob)
 
-		print(self.layer1.nodes())
+		elif self.layer2 == 'Layer1':
+			self.layer2 = self.layer1
 
-	def calculateInitialReputations(self):
-		initialReputations = ['Good' if random.choice([0,1]) == 0 else 'Bad' for _ in
-							 range(self.numNodes)]
-		return initialReputations
+		else:
+			print('Wrong layer2 parameter!')
+			exit()
+
+		self.nodePos = list(self.layer1.nodes())
+		for i in range(self.numNodes):
+			self.nodes.append({
+				'pos': self.nodePos[i], # Redundante?
+				'id': self.idIterator,
+				'payoff': 0,
+				#'reputation': self.calculateInitialReputation(), # Not used because each node has its perception of all others
+				'strategy': initialStrategies[i]
+			})
+
+			self.idToIndex[self.idIterator] = len(self.nodes) - 1
+			self.idIterator += 1
+
+		# Check nodes
+		# for i in range(self.numNodes):
+		#	print('Node:', self.nodes[i]['id'], '| Strategy:', self.nodes[i]['strategy'])
+
+		# Each node has its own perception of all others
+		for node in self.nodes:
+			node['perception'] = [{'reputation': self.calculateInitialReputation(), 'id': i['id']} for i in self.nodes]
+			# Example: Node number 3's perception of node number 7
+			# print(self.nodes[3]['perception'][7]['reputation'])
+
+	def calculateInitialReputation(self):
+		if random.choice([0,1]) == 0:
+			initialReputation = 'Good'
+		else:
+			initialReputation = 'Bad'
+
+		return initialReputation
 
 	def calculateInitialStrategies(self): # Pode ficar mais bonito
 		initialStrategies = []
 		for _ in range(self.numNodes):
 			rand = random.randrange(4)
 			if rand == 0:
-				initialStrategies.append('SternJudging')
+				initialStrategies.append('AllC')
 			elif rand == 1:
-				initialStrategies.append('SimpleStanding')
+				initialStrategies.append('AllD')
 			elif rand == 2:
-				initialStrategies.append('Shunning')
+				initialStrategies.append('Disc')
 			elif rand == 3:
-				initialStrategies.append('ImageScoring')
+				initialStrategies.append('pDisc')
 
 		return initialStrategies
-	
-'''
-	def reproduce_Social(self):  # social learning where nodes copy another node's strategy with a given probability if that node's payoff is higher
+
+	def runSimulation(self):
+		print('=====    Initiating simulation   ======')
+		LogsPerGen=[]
+		for i in range(self.numGenerations):
+			#print('-- Generation {} --'.format(i))
+			lg = self.runGeneration()
+			lg['generation'] = i
+			l = None
+
+			if i % self.logFreq == 0:
+				print('== Logging {} =='.format(i))
+				#l = self.LogsPerGen(i)
+				drawGraph(self.layer1, self.nodes, dir, i)
+
+			self.reproduceSocial()
+
+			if l != None:
+				lg.update(l)
+
+			LogsPerGen.append(lg)
+
+	def runGeneration(self):
+
+		interactionPairs = getNeighborPairs(self.layer1, self.nodes, self.nodePos)
+		actions = []
+		for j, pair in enumerate(interactionPairs):
+			actions.append(self.runInteraction(pair))
+
+		actionFreq = countFreq(actions)
+		cooperationRatio = actionFreq['cooperate'] if 'cooperate' in actionFreq.keys() else 0
+
+		# todo - Add stationary fraction of good and bad reputations
+		# # print(self.nodes)
+		return {'cooperationRatio': cooperationRatio}
+
+	def runInteraction(self, pair):
+		donor = pair[0]
+		recipient = pair[1]
+
+		if (donor['strategy'] == 'AllC') or \
+				(donor['strategy'] == 'Disc' and getRecipientReputation(donor, recipient) == 'Good')\
+				or donor['strategy'] == 'pDisc' and getRecipientReputation(donor, recipient) == 'Bad':
+			action = 'cooperate'
+			donor['payoff'] -= self.cost
+			recipient['payoff'] += self.benefit
+		else:
+			action = 'defect'
+
+		'''
+		# Verify if it's working properly
+		print(donor['strategy'])
+		print(getRecipientReputation(donor, recipient))
+		print(action)
+		'''
+		return action
+
+	def reproduceSocial(self):  # social learning where nodes copy another node's strategy with a given probability if that node's payoff is higher
+		interactionPairs = getNeighborPairs(self.layer1, self.nodes, self.nodePos)
 		beta = 10
 		for pair in interactionPairs:
 			mine = pair[0]
 			partner = pair[1]
-			if partner['payoff'] > mine['payoff']:
+			if partner['payoff'] > mine['payoff']: # fixme - is this correct or should I calculate de probability regardless?
 				prob = 1 / (1 + math.exp(-beta * (partner['payoff'] - mine['payoff'])))
 				if probability(prob):
-                	mine['strategy'] = partner['strategy']
-'''
+					mine['strategy'] = partner['strategy']
 
 if __name__ == "__main__":
 	# Variables used
 	initialValues = {
 		'numNodes': 100, # Number of nodes
-		'graphType': 'WattsStrogatz', # Graph topology: 'WattsStrogatz', 'Random', ...
-		'prob': 1, # Probability of rewiring links (WattsStrogatz)
-		'avgDegree': 2,
+		'prob': 0.2, # Probability of rewiring links (WattsStrogatz)
+		'avgDegree': 4,
+		'numGenerations': 10,
+		'logFreq': 1,
+		'cost': 0.1, # Cost of cooperation
+		'benefit': 1, # Benefit of receiving cooperation
+		'layer1': 'WattsStrogatz', # Graph topology: 'WattsStrogatz', 'Random', ...
+		'layer2': 'WattsStrogatz', # Graph topology: 'WattsStrogatz', 'Random', 'Layer1' (Layers are equal)
+
 	}
-	# Adicionar graphType1 e 2 para o layer 1 e 2 respectivamente
 
 	config = initialValues.copy()
-	sim = IndirectReciprocityMultiplexNetworks(**config)
+	dir = join('output', 'testrun{}'.format(1))
 
+	if not os.path.exists(dir):
+		mkdir(dir)
+
+	sim = IndirectReciprocityMultiplexNetworks(**config)
+	sim.runSimulation()
+
+	with open(join(dir, 'config.json'), 'w') as fp:
+		json.dump(config, fp)
+
+		# todo - Update perception and gossip: Start by making the recipient update, expand to select a random neighbor to update
+		# todo - Add social learning - Layer L1
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import networkx as nx
 import math
 import random
+import matplotlib.pyplot as plt
+from os.path import join
+from itertools import count
+
 
 class MultiplexNetwork(nx.Graph):
     # Generate a random graph with a given average degree and size.
@@ -19,6 +23,57 @@ class MultiplexNetwork(nx.Graph):
 def wattsStrogatz(numNodes, avgDegree, prob, rndSeed=None):
     G = nx.watts_strogatz_graph(numNodes,avgDegree, prob, seed=rndSeed)
     return G
+
+def getNeighborPairs(G, nodeInfo, pos):
+    # The index of each node in nodeInfo corresponds to the node with the same index in G.nodes
+    # For each node, get all of its neighbors
+    pairs = []
+    for it, n in enumerate(nodeInfo):
+        neighbors = G.neighbors(n['pos'])
+        for neighbor in neighbors:
+            neighborIt = pos.index(neighbor)
+            pairs.append([n, nodeInfo[neighborIt]])
+    random.shuffle(pairs)
+    return pairs
+
+def getRecipientReputation(donor, recipient):
+    return donor['perception'][recipient['id']]['reputation']
+
+def drawGraph(G, nodeInfo, dir, it):
+
+    # Group nodes according to their strategy
+    arr = ['AllC', 'AllD', 'Disc', 'pDisc']
+    groups = set(arr)
+    mapping = dict(zip(sorted(groups), count()))
+    nodes = G.nodes()
+    colors = [mapping[str(n['strategy'])] for n in nodeInfo]
+
+    # Drawing nodes and edges separately so we can capture collection for colorbar
+    #pos = nx.spring_layout(G)
+    pos = nx.circular_layout(G)
+    ec = nx.draw_networkx_edges(G, pos, alpha=0.2)
+    nc = nx.draw_networkx_nodes(G, pos, nodelist=nodes, node_color=colors, with_labels=False, node_size=50, cmap = plt.jet())
+    cbar = plt.colorbar(nc, ticks=[0, 1, 2, 3], orientation='vertical')
+    cbar.ax.set_yticklabels(['AllC', 'AllD', 'Disc', 'pDisc'])
+    # print(mapping) # Check if colorbar label is correct
+    plt.axis('off')
+    plt.savefig(join(dir, 'graph{}.png'.format(it)))
+    plt.close()
+
+def countFreq(arr):
+    mp = dict()
+
+    # Traverse through array elements and count frequencies
+    for i in range(len(arr)):
+        if arr[i] in mp.keys():
+            mp[arr[i]] += 1
+        else:
+            mp[arr[i]] = 1
+
+    # Normalize
+    for k in mp.keys():
+        mp[k] = mp[k]/len(arr)
+    return mp
 
 def probability(percentage):
     return (random.random() < percentage);
